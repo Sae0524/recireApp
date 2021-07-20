9c3606213add52d09eeeb9f3141321096ceb20fc
Sae0524 <ume.m3s@gmail.com>
2021-07-20 09:18:04 -0700

    finalCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from "react";
+import Recipe from "./Recipe";
 import './App.css';
+import { AiOutlineSearch } from "react-icons/ai";
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "2ea39878";
+  const APP_KEY = "6f58d953c487ee7c85f727e9c5cbb7e5";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+useEffect( () =>{
+  getRecipes();
+},[query]);
+
+const getRecipes = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`) 
+  const data = await response.json();
+  setRecipes(data.hits);
+};
+
+const updateSearch = e => {
+  setSearch(e.target.value);
+};
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search)
+  setSearch('');
+}
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Seach for any recipe!!</h2>
+      
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" 
+        type = "text" 
+        placeholder="Search recipe..."
+        value={search}
+        onChange={updateSearch}/>
+        <button className="search-button" type="submit"><AiOutlineSearch/></button>
+      </form>
+
+      <div className="recipes">
+      {recipes.map(recipe => (
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}/>
+      ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
